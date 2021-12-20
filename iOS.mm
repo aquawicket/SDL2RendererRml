5296eca3b40162b95a4da7016e08b1be23e5ea01
aquawicket <5370616+aquawicket@users.noreply.github.com>
2021-12-20 01:13:32 -0800

    removed redundant code

--- iOS.mm
+++ iOS.mm
@@ -8,37 +8,18 @@
     //NSBundle *bundle = [NSBundle mainBundle];
     //NSDictionary *info = [bundle infoDictionary];
     //NSString *prodName = [info objectForKey:@"CFBundleExecutable"];
-    //DKString appname = std::string([prodName UTF8String]); //NSString to std::string
+    //std::string appname = std::string([prodName UTF8String]); //NSString to std::string
     //NSString *epath = [[NSBundle mainBundle] executablePath];
-    //DKFile::exe_path = std::string([epath UTF8String]); //NSString to std::string
+    //std::string exe_path = std::string([epath UTF8String]); //NSString to std::string
 
     //NSString *documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
 	//NSString *sourcePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Populator"];
 	//NSString *folderPath = [documentsDirectory stringByAppendingPathComponent:@"Files"];
 	//NSLog(@"Source Path: %@\n Documents Path: %@ \n Folder Path: %@", sourcePath, documentsDirectory, folderPath); 
-    
-    //DKFile::appfilename = "/Users/aquawicket/Desktop/digitalknob/DKApps/"+appname+"/iossim32/Release-iphonesimulator/"+appname+".app/"+appname;
 	
     App app(0,0);  //argc, argv);
 	App::active = true;
-    
-	//DKStringArray classes;
-	//DKClass::GetClassList(classes);
-	//for(int i=0; i<classes.size(); i++){
-	//	DKINFO(classes[i]+"\n");
-	//}
-
-	//Attempt to preload these
-	//DKClass::DKCreate("DKAssets");
-	//DKClass::DKCreate("DKDuktape");
-
-	//DKObject* app = DKClass::DKCreate("App"); //App.h/App.cpp (user code)
-	//dkapp.Init();
-    
-    //DKApp dkapp(0,0);//argc, argv);
-    //DKApp::Init();
-    //DKApp::Loop();
-		
+    		
     [NSTimer scheduledTimerWithTimeInterval:1.0/30.0 target:self selector:@selector(updateScene) userInfo:nil repeats:YES];
 }
 
